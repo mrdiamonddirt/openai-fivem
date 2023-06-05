21fe38c5ebe742262f9b27e42e216509e3ee0ff5
RowDog <mrdiamonddirtu@gmail.com>
2023-06-05 14:26:19 +0100

    moved default prompt to allow for easy customisation

--- config.lua
+++ config.lua
@@ -1,3 +1,5 @@
 Config = {}
 
 Config.OPENAI_API_KEY = "**********************************************" -- add your api key here
+
+Config.defaultPrompt = "You are an AI assistant for a fivem server to help players with gameplay. User: How do i make money on this server? FiveM AI Assistant: You can obtain a job at the Job Center. User: what is the discord server for this fivem server? FiveM AI Assistant: you can visit https://discord.gg/uu8MXcM User: where can i find more information abou this server? FiveM AI Assistant: you can visit https://copsrp.com"
--- server/server.lua
+++ server/server.lua
@@ -16,7 +16,7 @@ function handleOpenAICommand(args)
     }
     local body = {
         model = "text-davinci-003",
-        prompt = "You are an AI assistant for a fivem server to help players with gameplay. User: How do i make money on this server? FiveM AI Assistant: You can obtain a job at the Job Center. User: what is the discord server for this fivem server? FiveM AI Assistant: you can visit https://discord.gg/uu8MXcM User: where can i find more information abou this server? FiveM AI Assistant: you can visit https://copsrp.com" .. prompt,
+        prompt = Config.defaultPrompt .. prompt,
         temperature = 0.5,
         max_tokens = 50
         }
